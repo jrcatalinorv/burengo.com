0a975a330040ddd41780e6df0fd55818d007598a
jrcatalinorv <jrcatalinorv@gmail.com>
2020-04-16 18:31:38 -0400

    version 1.0.0.2

--- cu/modelos/conexion.php
+++ cu/modelos/conexion.php
@@ -2,8 +2,6 @@
 class Conexion{
 	public function conectar(){
 		$link = new PDO("mysql:host=localhost;dbname=bgo_database_site","burengo","bgoAdmin7869");
-		//$link = new PDO("mysql:host=localhost;dbname=jrcatali_burengo","jrcatali_bgoAdm","bgoAdmin7869");
-		//$link = new PDO("mysql:host=localhost;dbname=dcdvoqzc_mpro","dcdvoqzc_admin","Dcdvoqzc@sytem88");	
 		$link->exec("set names utf8");
 		return $link;
 	}
